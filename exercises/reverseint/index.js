4c40c3ee1802535c8f46fe37bd9d7b937ca3dcf4
Kristin M. Oberlander <koberlander115@gmail.com>
2019-11-05 17:35:18 -0500

    added reverseInt solution with Math.sign()

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // convert n into a string, array, and reverse
+    let reverseNum = parseInt(n.toString().split('').reverse().join(''))
+
+  // handle negative numbers and zeros by adding the Math function, Math.sign()
+    return reverseNum * Math.sign(n)
+}
 
 module.exports = reverseInt;

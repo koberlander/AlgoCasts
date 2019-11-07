c8ee7373c95d25c6e4f1d02577018e130b66189f
Kristin M. Oberlander <koberlander115@gmail.com>
2019-11-07 16:18:16 -0500

    working through maxChar solution with notes

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // create an empty object for us to place each character in our string in
+  const charsObj = {}
+
+  // create a maximum variable and set it to 0 to start. We will change its value each time we find a new highest value in our object
+  let max = 0
+
+  // create a variable that keeps track of which character appears most. Set it to an empty string to start.
+  let maxChar = ''
+
+// iterate through each character in the string, make it a key and implement the value each time it appears
+  for(let char of str){
+    // if the character already exists, add one to the value
+    if (charsObj[char]) {
+      charsObj[char]++
+
+    //if it doesn't already exist, set the value to 1
+    } else {
+      charsObj[char] = 1
+    }
+    console.log(charsObj);
+ }
+}
 
 module.exports = maxChar;

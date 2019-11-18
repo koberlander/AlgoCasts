1983c69845a45f8d61dcb92ef3327065bcfd509c
Kristin M. Oberlander <koberlander115@gmail.com>
2019-11-18 14:43:10 -0500

    added my preferred solution with slice and a while loop

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,31 +8,54 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+//This was my first though for the solution to this problem
 function chunk(array, size) {
-  // create empty array to hold chunks called 'chunked'
+  // create empty chunked array
   const chunked = []
 
+  //create index variable and start at 0
+  let index = 0
 
-  // use for ... of to iterate through each element in the original array
-    for(let element of array ){
+    // while index is less than the length of the original array
+    while(index < array.length){
 
-      // find last element in chunked
-      const last = chunked[chunked.length - 1]
+      // create a variable called sliced that cuts from beginning of array to the size
+      let sliced = array.slice(index, index + size)
 
-      // if last element doesn't exist (bc there's nothing in that chunk yet) or the last element is equal to the size of the chunk
-        if (!last || last.length === size) {
+      // push that slice into chunked
+      chunked.push(sliced)
 
-          // push a new chunk into 'chunked' with the current element
-          chunked.push([element])
-
-      // otherwise, add the element to the current chunk
-        } else {
-          last.push(element)
-        }
+      index += size
     }
-
-  // return an array of arrays
-    return chunked
+  return chunked
 }
 
 module.exports = chunk;
+
+// ITERATIVE SOLUTION
+// function chunk(array, size) {
+//   // create empty array to hold chunks called 'chunked'
+//   const chunked = []
+//
+//
+//   // use for ... of to iterate through each element in the original array
+//     for(let element of array ){
+//
+//       // find last element in chunked
+//       const last = chunked[chunked.length - 1]
+//
+//       // if last element doesn't exist (bc there's nothing in that chunk yet) or the last element is equal to the size of the chunk
+//         if (!last || last.length === size) {
+//
+//           // push a new chunk into 'chunked' with the current element
+//           chunked.push([element])
+//
+//       // otherwise, add the element to the current chunk
+//         } else {
+//           last.push(element)
+//         }
+//     }
+//
+//   // return an array of arrays
+//     return chunked
+// }

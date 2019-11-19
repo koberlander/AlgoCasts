d4a286a871b705ebc18f5098f626076ea92999ec
Kristin M. Oberlander <koberlander115@gmail.com>
2019-11-19 12:57:26 -0500

    wrote out plan for solution plus a note on how to make code more DRY

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,25 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+// Save 'processor' to a variable so I can just call it on each string instead of writing it twice.
+
+    // use RegX to strip out punctuation and spaces, include upper and lower case in stringA
+    // turn stringA into an object
+    // loop thru stringA with a for...in loop
+      // count number of times each character appears in the string
+        // if the character already exists, add one to the counter (+= 1 or ++?)
+        // otherwise assign the character to a value of 0
+    // don't forget to return a value
+
+    // use RegX to strip out punctuation and spaces, included upper and lower case in stringB
+    // loop thru stringA with a for...in loop
+      // count number of times each character appears in the string
+        // if the character already exists, add one to the counter (+= 1 or ++?)
+        // otherwise assign the character to a value of 0
+    // don't forget to return a value
+
+    // return the direct comparison of strings (true or false)
+}
 
 module.exports = anagrams;

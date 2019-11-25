a9012705c88c32c2adf94d3749cbf0fd2b9e89dd
Kristin M. Oberlander <koberlander115@gmail.com>
2019-11-25 16:32:41 -0500

    solution using a for of loop to split, uppercase, and slice

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,17 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const words = []
+
+  // loop through array that has been split with a space, which will separate each word
+  for (word of str.split(' ')) {
+    // capitalize the first letter then concat the word at index 1 to the end
+    // push the result of the capitalization and join into the words array
+    words.push(word[0].toUpperCase() + word.slice(1))
+  }
+  // return the result of the 'joined' words array. once again we are joining with the space character instead of an empty string
+  return words.join(' ')
+}
 
 module.exports = capitalize;

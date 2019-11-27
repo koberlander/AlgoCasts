80d5db4d0b40de8b8110c9d2defb23033ad1d16d
Kristin M. Oberlander <koberlander115@gmail.com>
2019-11-27 15:53:34 -0500

    added 2nd solution that I think I like better

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,17 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
+// this solution relies on looping through each character in a string and capitalizing it if there is a space to the left of it, aka if it is at the beginning of a word
 function capitalize(str) {
-  const words = []
 
-  // loop through array that has been split with a space, which will separate each word
-  for (word of str.split(' ')) {
-    // capitalize the first letter then concat the word at index 1 to the end
-    // push the result of the capitalization and join into the words array
-    words.push(word[0].toUpperCase() + word.slice(1))
+  // the solution won't work on the first character in the string -- there's no space next to it -- so save to a variable the first character capitalized and add characters to it in the next section.
+  let result = str[0].toUpperCase()
+
+  // for each character in the string and beginning at index 1
+  for (let i = 1; i < str.length; i++) {
+    
+    // if the character minus 1 is a space, uppercase the character and add to result
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase()
+
+    // otherwise, just add it to result
+    } else {
+      result += str[i]
+    }
   }
-  // return the result of the 'joined' words array. once again we are joining with the space character instead of an empty string
-  return words.join(' ')
+  return result
 }
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   const words = []
+//
+//   // loop through array that has been split with a space, which will separate each word
+//   for (word of str.split(' ')) {
+//     // capitalize the first letter then concat the word at index 1 to the end
+//     // push the result of the capitalization and join into the words array
+//     words.push(word[0].toUpperCase() + word.slice(1))
+//   }
+//   // return the result of the 'joined' words array. once again we are joining with the space character instead of an empty string
+//   return words.join(' ')
+// }

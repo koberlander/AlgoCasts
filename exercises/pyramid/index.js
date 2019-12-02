a221c3c8f14ec0bf97d667d0ce5ca7a6e4aee049
Kristin M. Oberlander <koberlander115@gmail.com>
2019-12-02 17:51:54 -0500

    started coding sequence to check number of columns

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -19,20 +19,29 @@ function pyramid(n) {
   for (let row = 0; row < n; row++) {
     let level = ''
     // From 0 to ??? (iterate through columns)
-    // the difference between rows/columns increases like so: 2 rows, 3 columns... 3 rows, 5 columns... 4 rows, 7 columns
-        
+    // the difference between rows/columns increases like so: 2 rows, 3 columns... 3 rows, 5 columns... 4 rows, 7 columns - fibonacci sequence?
+
           // use above to figure out how many spaces to add before #... is there a way to mirror what's before and after center item?
 
+      for (let column = 0; column < level.length; column++) {
         // if should have a #, add a # to level string
+        if (condition) {
 
-
+        }
         // otherwise add a space to level string
+        else {
 
+        }
+      }
+
+
+
+
+        // log my pyramid string inside the initial for loop
+        console.log(level);
   }
 
 
-      // log my pyramid string inside the initial for loop
-      console.log(level);
 }
 
 module.exports = pyramid;

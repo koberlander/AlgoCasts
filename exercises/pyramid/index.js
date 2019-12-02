1aec2e7a5295b4a837e2a64809ca8229a209a22d
Kristin M. Oberlander <koberlander115@gmail.com>
2019-12-02 17:37:24 -0500

    started iterative solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -16,17 +16,23 @@
 
 function pyramid(n) {
   // From 0 to n (iterate through rows)
+  for (let row = 0; row < n; row++) {
+    let level = ''
+    // From 0 to ??? (iterate through columns)
+    // the difference between rows/columns increases like so: 2 rows, 3 columns... 3 rows, 5 columns... 4 rows, 7 columns
+        
+          // use above to figure out how many spaces to add before #... is there a way to mirror what's before and after center item?
 
-      // From 0 to ??? (iterate through columns)
-          // research: if 3 rows there are X columns, if 8 rows, there are X columns if 10 rows there are X columns
-            // use above to figure out how many spaces to add before #... is there a way to mirror what's before and after center item?
-
-          // if should have a #, add a # to level string
+        // if should have a #, add a # to level string
 
 
-          // otherwise add a space to level string
+        // otherwise add a space to level string
+
+  }
+
 
       // log my pyramid string inside the initial for loop
+      console.log(level);
 }
 
 module.exports = pyramid;

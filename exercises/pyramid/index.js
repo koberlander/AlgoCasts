bf6c2cf02dc9efd8bde1e7d2fe57db6bbc556c61
Kristin M. Oberlander <koberlander115@gmail.com>
2019-12-02 09:58:10 -0500

    pseudo code ideas

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,19 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // From 0 to n (iterate through rows)
+
+      // From 0 to ??? (iterate through columns)
+          // research: if 3 rows there are X columns, if 8 rows, there are X columns if 10 rows there are X columns
+            // use above to figure out how many spaces to add before #... is there a way to mirror what's before and after center item?
+
+          // if should have a #, add a # to level string
+
+
+          // otherwise add a space to level string
+
+      // log my pyramid string inside the initial for loop
+}
 
 module.exports = pyramid;

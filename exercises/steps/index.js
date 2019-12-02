dbd97a8b83a1083bd3b71ee23d831fda22d39a3a
Kristin M. Oberlander <koberlander115@gmail.com>
2019-12-01 20:03:56 -0500

    added full solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -25,9 +25,17 @@ function steps(n, row = 0, stair = '') {
 
     if (n === stair.length) {
       console.log(stair);
-    } else {
-
+      steps(n, row + 1)
+      return
     }
+
+    if (stair.length <= row) {
+      stair += '#'
+
+    } else {
+      stair += ' '
+    }
+    steps(n, row, stair)
 }
 
 module.exports = steps;

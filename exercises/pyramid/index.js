20290751c2fb3ce9bcaa0d9fbeef0ef7deee1d06
Kristin M. Oberlander <koberlander115@gmail.com>
2019-12-03 12:50:44 -0500

    added recursive solution with notes

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,31 +14,58 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2)
-
-  // From 0 to n (iterate through rows)
-  for (let row = 0; row < n; row++) {
-    let level = ''
-    // From 0 to ??? (iterate through columns)
-    // the difference between rows/columns increases like so: 2 rows, 3 columns... 3 rows, 5 columns... 4 rows, 7 columns - fibonacci sequence?
-
-      for (let column = 0; column < 2 * n - 1; column++) {
-
-        // if should have a #, add a # to level string
-        if (midpoint - row <= column && midpoint + row >= column) {
-          level += '#'
-        }
-        // otherwise add a space to level string
-        else {
-          level += ' '
-        }
-      }
-
-        // log my pyramid string inside the initial for loop
-        console.log(level);
+// RECURSIVE SOLUTION
+function pyramid(n, row = 0, level = '') {
+  // base case
+  if (row === n) {
+    return
   }
 
+  // advance to the next row if level is 'full'
+  if (level.length === n * 2 - 1) {
+    console.log(level);
+    return pyramid(n, row + 1)
+  }
+
+  // create a midpoint variable that equals the center-most value of the array
+  const midpoint = Math.floor((n * 2 -1) / 2 )
+
+  let add
+  // add the proper character to level
+  if (midpoint - row <= level.length && midpoint + row >= level.length) {
+    add = '#'
+  } else {
+    add = ' '
+  }
+
+  pyramid(n, row, level + add)
 }
 
 module.exports = pyramid;
+
+// ITERATIVE SOLUTION
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2)
+//
+//   // From 0 to n (iterate through rows)
+//   for (let row = 0; row < n; row++) {
+//     let level = ''
+//     // From 0 to ??? (iterate through columns)
+//
+//       for (let column = 0; column < 2 * n - 1; column++) {
+//
+//         // if should have a #, add a # to level string
+//         if (midpoint - row <= column && midpoint + row >= column) {
+//           level += '#'
+//         }
+//         // otherwise add a space to level string
+//         else {
+//           level += ' '
+//         }
+//       }
+//
+//         // log my pyramid string inside the initial for loop
+//         console.log(level);
+//   }
+//
+// }

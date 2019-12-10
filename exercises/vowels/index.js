2bc0a64f904910036d542997184dc48cb51b86ed
Kristin M. Oberlander <koberlander115@gmail.com>
2019-12-10 17:42:54 -0500

    used the match helper and a ternary to solve

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -9,25 +9,36 @@
 
 function vowels(str) {
 
+  // use the match method on my string and set to a var
+  // match returns every item that matches the regExp
+  const matches = str.match(/[aeiou]/gi)
 
-  // convert str to array to use with includes()
-  const array = str.split('')
-
-  // create a counter variable to increment for each vowel
-  let counter = 0
-
-  for (let letter of array) {
-
-    // if letter === AEIOU or aeiou increase counter
-    if (array.includes(/[AEIOUaeiou]/g)) {
-      counter += 1
-      console.log('counter: ', counter);
-    }
-
-  }
-  return counter
-
+  // do a ternary to return the length of matches, if the length exists, or 0
+  return matches ? matches.length : 0
 
 }
 
 module.exports = vowels;
+
+// function vowels(str) {
+//
+//
+//   // convert str to array to use with includes()
+//   const array = str.split('')
+//
+//   // create a counter variable to increment for each vowel
+//   let counter = 0
+//
+//   for (let letter of array) {
+//
+//     //this checks the entire array not the letter
+//     if (array.includes(/[AEIOUaeiou]/g)) {
+//       console.log('counter: ', counter);
+//       counter += 1
+//     }
+//
+//   }
+//   return counter
+//
+//
+// }

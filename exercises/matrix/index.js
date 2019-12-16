4ef31f1ff15d7a11cf172d07cb0bbfc405dac0c5
Kristin M. Oberlander <koberlander115@gmail.com>
2019-12-16 16:11:54 -0500

    started pseudo-coding my approach

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,31 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  // create an empty array of arrays called results
+
+  // create a counter variable starting at 1
+
+  // as long as (start column <= end column) AND (start row <= end column)
+
+    // loop deom start column to end column
+
+      // At results[start_row][i] assign counter variable
+
+      // increment counter
+
+  // increment start row
+
+  // Loop from start row to end row
+
+    // At results[i][end_column] assign counter variable
+
+    // increment counter
+
+  // Decrement end row
+
+  // ...repeat for other two sides
+  
+}
 
 module.exports = matrix;
